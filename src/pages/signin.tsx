8ee846f3e443bd77324a5c449bb51f8188035a43
Steven <zhangc2@illinois.edu>
2024-07-24 22:40:17 -0500

    Added global CSS import to signin and signup pages

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,4 +1,6 @@
 // src/pages/signin.tsx
+import '../app/globals.css';
+
 import { useState, FormEvent } from 'react';
 import { useRouter } from 'next/router';
 import supabase from '../lib/supabaseClient';
