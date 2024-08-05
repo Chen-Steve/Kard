1cf9bf06e1ea25cd1a5149e089dcdfd8cc5e8832
Steven <zhangc2@illinois.edu>
2024-08-05 13:27:52 -0500

    established users don't need to signin again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../app/globals.css';
@@ -7,18 +9,33 @@ import '../lib/fontAwesome';
 import { DragDropContext } from '@hello-pangea/dnd';
 import { Analytics } from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function AuthWrapper({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === 'authenticated' && router.pathname === '/') {
+      router.push('/dashboard');
+    }
+  }, [status, router]);
+
+  return <>{children}</>;
+}
+
+function MyApp({ Component, pageProps: { session: initialSession, ...pageProps } }: AppProps) {
   const handleDragEnd = () => {
     // Handle the drag end event
   };
 
   return (
-    <SessionProvider session={session}>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Component {...pageProps} />
-        <Analytics />
-        <ToastContainer />
-      </DragDropContext>
+    <SessionProvider session={initialSession}>
+      <AuthWrapper>
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <Component {...pageProps} />
+          <Analytics />
+          <ToastContainer />
+        </DragDropContext>
+      </AuthWrapper>
     </SessionProvider>
   );
 }
